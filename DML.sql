6b7f846ed78488c3ab96352b87fc7e6149a81e93
faizftw <faizfatwa030206@gmail.com>
2022-10-08 19:17:23 +0800

    -membuat database dan user -Menggunakan Auto increment dan not null -perbaikan syntax

--- DML.sql
+++ DML.sql
@@ -1,30 +1,30 @@
-SELECT * FROM course
-INSERT INTO course (id_course, NAME, duration)
-VALUES (1, 'Analisis dan Perancangan Sistem', 100),
-		 (2, 'Pendidikan Agama', 90),
-		 (3, 'Struktur Data', 150),
-		 (4, 'Struktur Diskrit', 150),
-		 (5, 'Sistem Basis data I', 100);
+INSERT INTO course (NAME, duration)
+VALUES ('Analisis dan Perancangan Sistem', 100),
+		 ('Pendidikan Agama', 90),
+		 ('Struktur Data', 150),
+		 ('Struktur Diskrit', 150),
+		 ('Sistem Basis data I', 100);
+SELECT * FROM course;
 
-SELECT * FROM jadwal
-INSERT INTO jadwal (id_jadwal, id_course, tgl, jam)
-VALUES (1, 1, '2022-10-03', '2022-10-03 07:30:00'),
-		 (2, 2, '2022-10-04', '2022-10-04 16:20:00'),
-		 (3, 3, '2022-10-05', '2022-10-05 07:30:00'),
-		 (4, 4, '2022-10-05', '2022-10-05 10:10:00'),
-		 (5, 5, '2022-10-06', '2022-10-06 10:10:00');
+INSERT INTO jadwal (id_course, tgl, jam)
+VALUES (1, '2022-10-03', '2022-10-03 07:30:00'),
+		 (2, '2022-10-04', '2022-10-04 16:20:00'),
+		 (3, '2022-10-05', '2022-10-05 07:30:00'),
+		 (4, '2022-10-05', '2022-10-05 10:10:00'),
+		 (5, '2022-10-06', '2022-10-06 10:10:00');
+SELECT * FROM jadwal;
 
-SELECT * FROM students
-INSERT INTO students (id_students, full_name, address, major)
-VALUES (1, 'Djulizah Bonita Lanini', 'Jl. Permata Indah', 'Sistem Informasi'),
-		 (2, 'Dewa Rescue Virgiawansyah', 'Jl. Cempaka', 'Sistem Informasi'),
-		 (3, 'Firmansyah', 'Jl. Delima No.7', 'Sistem Informasi'),
-		 (4, 'Muhammad Dzacky ilham', 'Jl. Macan', 'Sistem Informasi'),
-		 (5, 'Muhammad Faiz Fatwa Syarifuddin', 'Jl. Anggrek Blok B No.1', 'Sistem Informasi'),
-		 (6, 'Muhammad Raihan aradhana', 'Jl. Pengayoman', 'Sistem Informasi'),
-		 (7, 'Wd. Ananda Lesmono', 'Bukit Baruga', 'Sistem Informasi');
+INSERT INTO students (full_name, address, major)
+VALUES ('Djulizah Bonita Lanini', 'Jl. Permata Indah', 'Sistem Informasi'),
+		 ('Dewa Rescue Virgiawansyah', 'Jl. Cempaka', 'Sistem Informasi'),
+		 ('Firmansyah', 'Jl. Delima No.7', 'Sistem Informasi'),
+		 ('Muhammad Dzacky ilham', 'Jl. Macan', 'Sistem Informasi'),
+		 ('Muhammad Faiz Fatwa Syarifuddin', 'Jl. Anggrek Blok B No.1', 'Sistem Informasi'),
+		 ('Muhammad Raihan aradhana', 'Jl. Pengayoman', 'Sistem Informasi'),
+		 ('Wd. Ananda Lesmono', 'Bukit Baruga', 'Sistem Informasi');
+SELECT * FROM students;
 
-SELECT * FROM students_jadwal
+SELECT * FROM students_jadwal;
 INSERT INTO students_jadwal (id_students, id_jadwal)
 VALUES (1, 1),
 		 (1, 2),
