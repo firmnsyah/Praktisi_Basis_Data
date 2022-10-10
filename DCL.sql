3755b066d864d2f83c1617b6520218909b778a2f
excradha <realradha76@gmail.com>
2022-10-10 11:11:47 +0800

    dcl

---
+++ DCL.sql
@@ -0,0 +1,8 @@
+-- Memberikan semua hak akses ke user
+GRANT ALL PRIVILEGES ON Kelompok_VB TO 'Kel_Vb'@'localhost';
+
+-- Memberikan hak akses tertentu (INSERT) ke user
+GRANT INSERT ON Kelompok_VB TO 'Kel_Vb'@'localhost';
+
+-- Mencabut semua hak akses dari user
+REVOKE ALL PRIVILEGES ON Kelompok_VB FROM 'Kel_Vb'@'localhost';
